31b5aa5c0b80bb05afbfd7839ef80eeba6e238f2
userlan <raushangul714@gmail.com>
2023-03-08 12:32:38 +0600

    3.1 If/Else - правильная ступенька: if ... elif... elif ... elif ... else(описание исключения)

--- quadratic.py
+++ quadratic.py
@@ -4,14 +4,15 @@ def solve(a, b, c):
     d = b*b - 4*a*c
     if d < 0:
         print("No solutions")
+    elif d ==0:
+        x = -b / (2*a)
+        print("One solution " + str(x))
+    elif d > 0:
+        x1 = (-b + sqrt(d)) / (2*a)
+        x2 = (-b - sqrt(d)) / (2 * a)
+        print("Two solutions " + str(x1) + " and " + str(x2))
     else:
-        if d ==0:
-            x = -b / (2*a)
-            print("One solution " + str(x))
-        else:
-            x1 = (-b + sqrt(d)) / (2*a)
-            x2 = (-b - sqrt(d)) / (2 * a)
-            print("Two solutions " + str(x1) + " and " + str(x2))
+        print("A-A-A that is impossible")
 
 solve(1, 1, 1)
 solve(1, 2, 1)
