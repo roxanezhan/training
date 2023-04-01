7e21323ebb65a4a31aff00fbf95662710e7af843
userlan <raushangul714@gmail.com>
2023-04-01 18:42:45 +0600

    4.5_lambda

--- geometry.py
+++ geometry.py
@@ -1,8 +1,17 @@
 
 from geom2d.point import Point
 
-l1 = [Point(0, 0), Point(1, 2), Point(2, 1)]
+l1 = [Point(3, 1), Point(0, 0), Point(1, 2)]
 
-l2 = sorted(l1)
+#def x(p): #это функция координаты х
+    #return p.x
+
+#def y(p): #это функция координаты у
+    #return p.y
+
+#l2 = sorted(l1, key=x)
+
+#l2 = sorted(l1, key = lambda p: p.x) # можно обратиться к кординате без ее объевления через функцию, используя ламбду
+l2 = sorted(l1, key = lambda p: p.distance(Point(0,0))) #сравнить по расстоянию от коррдинаты 0,0
 print(l1)
 print(l2)
